f7946852948e3712dda5fbff4f79b16d17fcbe98
Jeremy Chai <jeremy.chai@antra.com>
2023-08-25 06:12:37 -0400

    added backup plan for front end

---
+++ slit.py
@@ -0,0 +1,168 @@
+import joblib
+import streamlit as st
+import pandas as pd
+import random
+import numpy as np
+
+num_of_company = 1
+min_rev, max_rev = 500000, 90000000
+esg_cols = 18
+
+
+def generate_input_df():
+    data = {
+        'I1: Company Name': ['Company Example'],
+        'I2: Number of Employee': [random.randint(50, 2000) for _ in range(num_of_company)],
+        'I3: Revenue': [random.randint(min_rev, max_rev) for _ in range(num_of_company)],
+        'I4: Year': [2023],
+        'E1: Carbon emissions and intensity': [random.randint(1, 100) for _ in range(num_of_company)],
+        'E2: Energy consumption and renewable energy use': [random.randint(1, 100) for _ in range(num_of_company)],
+        'E3: Water usage and management': [random.randint(1, 100) for _ in range(num_of_company)],
+        'E4: Waste generation and recycling rates': [random.randint(1, 100) for _ in range(num_of_company)],
+        'E5: Air and water pollution levels': [random.randint(1, 100) for _ in range(num_of_company)],
+        'E6: Biodiversity conservation efforts': [random.randint(1, 100) for _ in range(num_of_company)],
+        'S1: Employee diversity and inclusion': [random.randint(1, 100) for _ in range(num_of_company)],
+        'S2: Labor practices and working conditions': [random.randint(1, 100) for _ in range(num_of_company)],
+        'S3: Health and safety performance': [random.randint(1, 100) for _ in range(num_of_company)],
+        'S4: Employee turnover and satisfaction': [random.randint(1, 100) for _ in range(num_of_company)],
+        'S5: Community engagement and philanthropic activities': [random.randint(1, 100) for _ in
+                                                                  range(num_of_company)],
+        'S6: Product quality and safety': [random.randint(1, 100) for _ in range(num_of_company)],
+        'G1: Board diversity and composition': [random.randint(1, 100) for _ in range(num_of_company)],
+        'G2: Executive compensation and incentives': [random.randint(1, 100) for _ in range(num_of_company)],
+        'G3: Transparency of executive pay ratios': [random.randint(1, 100) for _ in range(num_of_company)],
+        'G4: Shareholder rights and engagement': [random.randint(1, 100) for _ in range(num_of_company)],
+        'G5: Ethical business conduct and anti-corruption policies': [random.randint(1, 100) for _ in
+                                                                      range(num_of_company)],
+        'G6: Risk management and regulatory compliance': [random.randint(1, 100) for _ in range(num_of_company)]
+    }
+
+    return pd.DataFrame(data)
+
+def get_top_bottom(df):
+    columns_to_standardize = df.columns.tolist()[4:4+esg_cols]
+    columns_to_rank = [col for col in df.columns if 'Difference' in col]
+    kv = {col[:2]:col for col in columns_to_standardize}
+    ranked_df = df[columns_to_rank].apply(lambda row: row.rank(ascending=False), axis=1)
+
+    bottom_three = ranked_df.apply(lambda row: row.nlargest(3).index.tolist(), axis=1).to_list()[0]
+    top_three = ranked_df.apply(lambda row: row.nsmallest(3).index.tolist(), axis=1).to_list()[0]
+
+    bottom_three_col = [kv[c[:2]] for c in bottom_three]
+    top_three_col = [kv[c[:2]] for c in top_three]
+
+    t = ['Top 1', 'Top 2', 'Top 3', 'Bottom 3', 'Bottom 2', 'Bottom 1']
+
+    ret = pd.DataFrame({k:[v] for (k, v) in zip(t, top_three_col + bottom_three_col[::-1])})
+
+    return ret
+
+def get_difference(df):
+    # Calculate weighted ESG score for each row
+    weights = {'E': 0.4, 'S': 0.3, 'G': 0.3}
+    columns_to_standardize = df.columns.tolist()[4:4+esg_cols]
+    for k in weights.keys():
+        name = k + ' Average Score'
+        lst = [col for col in columns_to_standardize if col[0] == k]
+        size = len(lst)
+        df[name] = df.apply(lambda row: sum(row[col] for col in columns_to_standardize if col[0] == k) / size, axis=1)
+        for cur in lst:
+            cur_name = cur[:2] + ' Difference than Average'
+            df[cur_name] = df[cur] - df[name]
+    return df
+
+def load_models():
+    esg = joblib.load('assets/linear_ESG_model.pkl')
+    stock = joblib.load('assets/linear_Stock_model.pkl')
+    stock_esg = joblib.load('assets/linear_Stock_ESG_model.pkl')
+    good_news = joblib.load('assets/linear_ESG_Score_GoodNews.pkl')
+    bad_news = joblib.load('assets/linear_ESG_Score_BadNews.pkl')
+    return esg, stock, stock_esg, good_news, bad_news
+
+def format_stock(arr):
+    headers = ['Average', 'Minimum', 'Maximum']
+    return [h + ': $' + str(s) for h, s in zip (headers, np.round(arr, 2))]
+
+def make_predictions(df):
+    esg, stock, stock_esg, good, bad = load_models()
+    columns_to_standardize = df.columns.tolist()[4:4+esg_cols]
+    predicted_esg_score = esg.predict(df[columns_to_standardize])
+    predicted_stock = stock.predict(df['I3: Revenue'].to_numpy().reshape(-1, 1))
+    predicted_stock_esg = stock_esg.predict(df[columns_to_standardize + ['I3: Revenue']])
+    predicted_good = good.predict(predicted_esg_score[0].reshape(-1, 1))
+    predicted_bad = bad.predict(predicted_esg_score[0].reshape(-1, 1))
+    pred = {
+        'Predicted ESG Score' : [predicted_esg_score[0]],
+        'Predicted Stock Price from Revenue': [format_stock(predicted_stock[0])],
+        'Predicted Stock Price from Revenue with ESG': [format_stock(predicted_stock_esg[0])],
+        'Predicted Number of GOOD News Coverage': [int(predicted_good[0])],
+        'Predicted Number of BAD News Coverage': [int(predicted_bad[0])]
+    }
+    return pd.DataFrame(pred).round(2)
+def get_suggestion(df, point):
+    columns_to_standardize = df.columns.tolist()[4:4+esg_cols]
+    columns_to_rank = [col for col in df.columns if 'Difference' in col]
+    kv = {col[:2]:col for col in columns_to_standardize}
+    ranked_df = df[columns_to_rank].apply(lambda row: row.rank(ascending=False), axis=1)
+
+    bottom_three = ranked_df.apply(lambda row: row.nlargest(3).index.tolist(), axis=1).to_list()[0]
+    bottom_three_col = [kv[c[:2]] for c in bottom_three]
+
+    df[bottom_three_col] += point
+
+    ret = make_predictions(df)
+
+    return ret
+def delete_input():
+    if 'data' in st.session_state:
+        del st.session_state['data']
+
+st.write("""
+# ESG For Everyone - Challenge 1
+""")
+
+st.write("""
+## Enter Your Company ESG Info For Suggestions
+""")
+# col1, col2 = st.columns([1, 3])
+# with col1:
+#     generate = st.button('Generate Data', on_click=delete_input())
+# with col2:
+#     clear = st.button('Empty Form', on_click=delete_input())
+
+inputs = {}
+
+# if generate:
+#     gen_df = generate_input_df()
+#     kv = gen_df.to_dict(orient='records')[0].items()
+#     with st.form('geninput'):
+#         for k, v in kv:
+#             inputs[k] = [st.text_input(k, v)]
+#         st.session_state['data'] = inputs
+#         submitted = st.form_submit_button("Submit")
+# #
+# # if clear:
+# else:
+gen_df = generate_input_df()
+kv = gen_df.columns.tolist()
+with st.form('userinput'):
+    for k in kv:
+        inputs[k] = [st.text_input(k, '')]
+    submitted = st.form_submit_button("Submit")
+    st.session_state['data'] = inputs
+
+    if submitted:
+        df_input = pd.DataFrame(st.session_state['data'])
+
+        pred = make_predictions(df_input)
+        diff_df = get_difference(df_input.copy())
+        top_bottom = get_top_bottom(diff_df)
+        point = random.randint(1, 10)
+        suggest = get_suggestion(diff_df, point)
+
+        st.dataframe(df_input, top_bottom, suggest)
+
+
+#
+# if 'data' in st.session_state:
+#     print(df_input)
